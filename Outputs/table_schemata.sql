d2c36770f61e9c20c4ede8cd031adda1659feb9c
JeniLam <jenilam639@gmail.com>
2020-10-24 21:43:23 -0500

    Adds PK to tables which allow

--- Outputs/table_schemata.sql
+++ Outputs/table_schemata.sql
@@ -3,15 +3,22 @@ create table departments (
 	dept_name VARCHAR (50) not null
 );
 
+select * from departments
+
 create table dept_emp (
 	emp_no int not null,
 	dept_no VARCHAR (4) not null
 );
 
+
+select * from dept_emp
+
 create table dept_manager(
 	dept_no VARCHAR (4) not null,
 	emp_no in not null
 );
+
+select * from dept_manager
 
 --https://www.w3schools.com/sql/sql_datatypes.asp
 create table employees (
@@ -24,12 +31,39 @@ create table employees (
 	hire_date DATE
 );
 
+select * from employees
+
 create table salaries (
 	emp_no int not null,
 	salary int not null
 );
 
+select * from salaries
+
 create table titles (
 	title_id varchar not null,
 	title varchar not null
 );
+
+select * from titles
+
+-- https://www.w3schools.com/sql/sql_primarykey.ASP
+ALTER TABLE departments
+ADD PRIMARY KEY (dept_no);
+
+--ALTER TABLE dept_emp
+--ADD PRIMARY KEY (emp_no);
+--tried to run and found emp_no 10010 was duplicate
+
+--ALTER TABLE dept_manager
+--ADD PRIMARY KEY (dept_no);
+--tried to run discovered d001 is duplicate
+
+ALTER TABLE employees
+ADD PRIMARY KEY (emp_no);
+
+ALTER TABLE salaries
+ADD PRIMARY KEY (emp_no);
+
+ALTER TABLE titles
+ADD PRIMARY KEY (title_id);
