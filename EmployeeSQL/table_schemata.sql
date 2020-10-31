dfb7624eccc5f24f170ffc043351e132d660f8af
JeniLam <jenilam639@gmail.com>
2020-10-30 21:57:20 -0500

    Updates Schema, erd and starts analysis

--- EmployeeSQL/table_schemata.sql
+++ EmployeeSQL/table_schemata.sql
@@ -1,58 +1,68 @@
--- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
--- Link to schema: https://app.quickdatabasediagrams.com/#/d/qMIFMq
--- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
-
-
-CREATE TABLE "Departments" (
-    "dept_no" VARCHAR(15)   NOT NULL,
-    "dept_name" VARCHAR(255)   NOT NULL,
-    CONSTRAINT "pk_Departments" PRIMARY KEY (
-        "dept_no"
-     )
+create table departments (
+	dept_no VARCHAR (15) not null,
+	dept_name VARCHAR (255) not null
 );
 
-CREATE TABLE "Department_Employee" (
-    "emp_no" INT   NOT NULL,
-    "dept_no" VARCHAR(15)   NOT NULL,
-    CONSTRAINT "pk_Department_Employee" PRIMARY KEY (
-        "emp_no","dept_no"
-     )
+select * from departments;
+
+create table dept_emp (
+	emp_no INT not null,
+	dept_no VARCHAR (15) not null
 );
 
-CREATE TABLE "Department_Manager" (
-    "dept_no" VARCHAR(15)   NOT NULL,
-    "emp_no" INT   NOT NULL,
-    CONSTRAINT "pk_Department_Manager" PRIMARY KEY (
-        "dept_no","emp_no"
-     )
+select * from dept_emp
+
+create table dept_manager(
+	dept_no VARCHAR (15) not null,
+	emp_no INT not null
 );
 
-CREATE TABLE "Employees" (
-    "emp_no" INT   NOT NULL,
-    "emp_title" VARCHAR(255)   NOT NULL,
-    "birth_date" DATE   NOT NULL,
-    "first_name" VARCHAR(255)   NOT NULL,
-    "last_name" VARCHAR(255)   NOT NULL,
-    "sex" CHAR   NOT NULL,
-    "hire_date" DATE   NOT NULL,
-    CONSTRAINT "pk_Employees" PRIMARY KEY (
-        "emp_no"
-     )
+select * from dept_manager
+
+--https://www.w3schools.com/sql/sql_datatypes.asp
+create table employees (
+	emp_no INT not null,
+	emp_title_id VARCHAR (255) NOT NULL,
+	birth_date DATE,
+	first_name VARCHAR (255) not null,
+	last_name VARCHAR (255) not null,
+	sex CHAR,
+	hire_date DATE
 );
 
-CREATE TABLE "Salaries" (
-    "emp_no" INT   NOT NULL,
-    "salary" INT   NOT NULL,
-    CONSTRAINT "pk_Salaries" PRIMARY KEY (
-        "emp_no"
-     )
+select * from employees
+
+create table salaries (
+	emp_no INT not null,
+	salary INT not null
 );
 
-CREATE TABLE "Titles" (
-    "title_id" VARCHAR(15)   NOT NULL,
-    "title" VARCHAR(255)   NOT NULL,
-    CONSTRAINT "pk_Titles" PRIMARY KEY (
-        "title_id"
-     )
+select * from salaries
+
+create table titles (
+	title_id VARCHAR (255) not null,
+	title VARCHAR (255) not null
 );
 
+select * from titles
+
+-- https://www.w3schools.com/sql/sql_primarykey.ASP
+ALTER TABLE departments
+ADD PRIMARY KEY (dept_no);
+
+ALTER TABLE dept_emp
+ADD PRIMARY KEY (emp_no, dept_no);
+--tried to run and found emp_no 10010 was duplicate
+
+ALTER TABLE dept_manager
+ADD PRIMARY KEY (dept_no, emp_no);
+--tried to run discovered d001 is duplicate
+
+ALTER TABLE employees
+ADD PRIMARY KEY (emp_no);
+
+ALTER TABLE salaries
+ADD PRIMARY KEY (emp_no);
+
+ALTER TABLE titles
+ADD PRIMARY KEY (title_id);
---
+++ data/Data_Analysis.sql
@@ -0,0 +1,15 @@
+--Data Analysis
+
+
+--1. List the following details of each employee: employee number, last name, first name, sex, and salary.
+select employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary
+from employees inner join salaries on employees.emp_no = salaries.emp_no;
+
+--2.List first name, last name, and hire date for employees who were hired in 1986.
+select first_name, last_name, hire_date from Employees
+WHERE hire_date >= 1986-01-01 AND <= 1986-12-31;
+
+--3.List the manager of each department with the following information: 
+--department number, department name, the manager's employee number, last name, first name.
+Select dept_
+select *from dept_manager
