23ce8845403507d761e3c96df9c440c501b135a1
JeniLam <jenilam639@gmail.com>
2020-10-30 22:24:18 -0500

    Adds code for ?'s 3 & 5

--- data/Data_Analysis.sql
+++ data/Data_Analysis.sql
@@ -11,5 +11,32 @@ WHERE hire_date >= 1986-01-01 AND <= 1986-12-31;
 
 --3.List the manager of each department with the following information: 
 --department number, department name, the manager's employee number, last name, first name.
-Select dept_
-select *from dept_manager
+--from day 3 activity 6
+SELECT dm.dept_no, d.dept_name, dm.emp_no, e.last_name, e.first_name 
+FROM dept_manager AS dm
+INNER JOIN departments AS d
+ON dm.dept_no=d.dept_no
+LEFT JOIN employees AS e
+ON dm.emp_no=e.emp_no;
+
+
+--4.List the department of each employee with the following information: 
+--employee number, last name, first name, and department name.
+
+
+--5. List first name, last name, and sex for employees whose 
+--first name is "Hercules" and last names begin with "B."
+
+select first_name, last_name, sex from Employees
+WHERE first_name = 'Hercules' and last_name like 'B%';
+
+--6. List all employees in the Sales department, including 
+--their employee number, last name, first name, and department name.
+
+--7. List all employees in the Sales and Development departments, 
+--including their employee number, last name, first name, and department name.
+
+--8. In descending order, list the frequency count of 
+--employee last names, i.e., how many employees share each last name.
+
+
