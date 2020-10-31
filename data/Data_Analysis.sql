88a157ba6ffb8600c9090a88d38c768a3c286bb9
JeniLam <jenilam639@gmail.com>
2020-10-31 10:02:21 -0500

    Add's ? 4 code

--- data/Data_Analysis.sql
+++ data/Data_Analysis.sql
@@ -7,7 +7,7 @@ FROM employees INNER JOIN salaries ON employees.emp_no = salaries.emp_no;
 
 --2.List first name, last name, and hire date for employees who were hired in 1986.
 SELECT first_name, last_name, hire_date FROM Employees
-WHERE hire_date >= 1986-01-01 AND <= 1986-12-31;
+WHERE hire_date >= '1986-01-01' AND hire_date <= '1986-12-31';
 
 --3.List the manager of each department with the following information: 
 --department number, department name, the manager's employee number, last name, first name.
