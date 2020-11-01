79f951e8a255d2ee900c726b81e011e28b0da27a
JeniLam <jenilam639@gmail.com>
2020-10-31 20:49:01 -0500

    Adds ?'s 6 & 7 Code

--- data/Data_Analysis.sql
+++ data/Data_Analysis.sql
@@ -35,11 +35,23 @@ ON de.dept_no=d.dept_no;
 SELECT first_name, last_name, sex FROM Employees
 WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
 
---6. List all employees in the Sales department, including 
---their employee number, last name, first name, and department name.
+--6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees AS e
+INNER JOIN dept_emp AS de
+ON e.emp_no = de.emp_no
+INNER JOIN departments AS d
+ON de.dept_no=d.dept_no
+WHERE dept_name = 'Sales' ;
 
---7. List all employees in the Sales and Development departments, 
---including their employee number, last name, first name, and department name.
+--7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees AS e
+INNER JOIN dept_emp AS de
+ON e.emp_no = de.emp_no
+INNER JOIN departments AS d
+ON de.dept_no=d.dept_no
+WHERE dept_name = 'Sales' OR dept_name = 'Development';
 
 --8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
 --day 3 activity 3
